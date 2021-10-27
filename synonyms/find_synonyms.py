f58a9b5165f267c5877cd37b904e65ffb5b628e4
Daniel Gregory <daniel.gregory3@outlook.com>
2021-10-27 14:25:06 +0100

    Refactor in preperation for testing.

---
+++ synonyms/find_synonyms.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+
+import synonyms.http.client as http_client
+
+THESAURUS_BASE_URL = 'https://www.thesaurus.com/browse/'
+
+
+class FindSynonyms:
+
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def get_synonyms(self, word):
+        url = FindSynonyms._construct_url(self.base_url, word)
+        html_text = http_client.get_html(url)
+
+        soup = FindSynonyms._to_soup(html_text)
+
+        meanings = soup.find(id='meanings')
+        return list(map(lambda node: node.text, meanings.find_all('a')))
+
+    @staticmethod
+    def _construct_url(base_url, word):
+        return base_url + word
+
+    @staticmethod
+    def _to_soup(html_text: str):
+        return BeautifulSoup(html_text, 'html.parser')

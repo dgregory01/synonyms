0c788cf17c48e0d0a354d748a9c8b8d9797e5ca9
Daniel Gregory <daniel.gregory3@outlook.com>
2021-10-27 15:00:17 +0100

    Use Pytest fixture as reference to stubbed HTML.

--- tests/test_synonyms.py
+++ tests/test_synonyms.py
@@ -1,17 +1,31 @@
 import pkgutil
 
+import pytest
 from doubles import allow
 
 from synonyms.find_synonyms import FindSynonyms
 from synonyms.find_synonyms import http_client as mock_http_client
 
 
+# Test set up functions #
+
 def _read_resource_to_str(resource: str):
     return pkgutil.get_data(__name__, 'resources/' + resource)
 
 
-def test_list_synonyms_happy_path():
-    allow(mock_http_client).get_html.and_return(_read_resource_to_str('synonyms.html'))
+@pytest.fixture
+def synonyms_html() -> str:
+    """
+    :return: stubbed HTML response from synonyms URL.
+    :rtype Str
+    """
+    return _read_resource_to_str('synonyms.html')
+
+
+# Unit tests below #
+
+def test_list_synonyms_happy_path(synonyms_html):
+    allow(mock_http_client).get_html.and_return(synonyms_html)
 
     soup = FindSynonyms('any url')
     synonyms = soup.get_synonyms('happy')

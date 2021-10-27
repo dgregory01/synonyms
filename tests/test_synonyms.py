f63309c23e340eee72673a3b0af09bcf809f4f1f
Daniel Gregory <daniel.gregory3@outlook.com>
2021-10-27 14:50:37 +0100

    Addition of unit tests.

--- tests/test_synonyms.py
+++ tests/test_synonyms.py
@@ -1,21 +1,21 @@
+import pkgutil
+
 from doubles import allow
 
-# from synonyms.find_synonyms import FindSynonyms
 from synonyms.find_synonyms import FindSynonyms
+from synonyms.find_synonyms import http_client as mock_http_client
 
 
-# from tests.utils import read_resource_to_str
+def _read_resource_to_str(resource: str):
+    return pkgutil.get_data(__name__, 'resources/' + resource)
 
 
-def test_me():
-    assert 5 == 5
+def test_list_synonyms_happy_path():
+    allow(mock_http_client).get_html.and_return(_read_resource_to_str('synonyms.html'))
 
-# def test_list_synonyms_happy_path():
-#     allow(mock_http_client).get.and_return(read_resource_to_str('synonyms.html'))
-#
-#     soup = FindSynonyms('any url')
-#     synonyms = soup.get_synonyms('happy')
-#
-#     assert len(synonyms) == 48
-#     assert synonyms[0] == 'cheerful'
-#     assert synonyms[47] == 'walking on air'
+    soup = FindSynonyms('any url')
+    synonyms = soup.get_synonyms('happy')
+
+    assert len(synonyms) == 48
+    assert synonyms[0] == 'cheerful '
+    assert synonyms[47] == 'walking on air '
